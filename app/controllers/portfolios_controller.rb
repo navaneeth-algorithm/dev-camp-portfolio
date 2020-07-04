3970fa8900c27c3a1ddc04006f3060c3d0fb0568
navaneeth <navaneethb807@gmail.com>
2020-07-04 19:11:40 +0530

    Edit,delete & show features to portfolio is added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,4 +18,34 @@ class PortfoliosController < ApplicationController
             
         end
     end
+
+    def show
+        @portfolio = Portfolio.find(params[:id])
+    end
+
+    def edit
+        @portfolio = Portfolio.find(params[:id])
+        @url = params
+    end
+
+    def destroy
+        @portfolio = Portfolio.find(params[:id])
+        @portfolio.destroy
+        respond_to do |format|
+          format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully deleted.' }
+          format.json { head :no_content }
+        end
+    
+    end
+
+    def update
+        @portfolio = Portfolio.find(params[:id])
+        respond_to do |format|
+            if @portfolio.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+                format.html {  redirect_to portfolios_path, notice: 'Portfolio updated'  }
+            else
+                format.html { redirect_to :edit }
+            end
+        end
+    end
 end
