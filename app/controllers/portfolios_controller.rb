85d65c06261bc5c5013672cb4fb65968f828547a
navaneeth <navaneethb807@gmail.com>
2020-07-04 18:00:10 +0530

    new create action portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,20 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio = Portfolio.all
     end
+
+    def new
+        @portfolio = Portfolio.new
+    end
+
+    def create
+        @portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+        respond_to do |format|
+            if @portfolio.save
+                format.html { redirect_to portfolios_path, notice: 'Portfolio created' }
+            else
+                format.html {redirect_to :new}
+            end
+            
+        end
+    end
 end
